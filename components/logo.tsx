5e9c16f5da4f76cc77766cd73881c31cbcd99d32
emoonchain <emoonchain@gmail.com>
2026-04-14 10:36:31 +0000

    Upload ulang seluruh struktur proyek

--- components/logo.tsx
+++ components/logo.tsx
@@ -8,8 +8,8 @@ return(
          <div className="relative items-center justify-center mx-auto mb-4 w-12 h-12 md:w-14 md:h-14">
                         <Image
                         
-                          src="https://nmzg68mby1os258h.public.blob.vercel-storage.com/logo_web-deCCmxPsDLRIzi8JDbtrB3uecKPD9q.png"
-                          alt="DoMesh Store Logo"
+                          src="https://nmzg68mby1os258h.public.blob.vercel-storage.com/logo_1-jUsDzBzgtlctx4zsJ4BmfwLg3IAqG0.png"
+                          alt="BANGDIM Store Logo"
                           fill
                           className="object-contain"
                           sizes="(max-width: 768px) 48px, 56px"
